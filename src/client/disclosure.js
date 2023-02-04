dc1182b46a55d9b1fac0c5449ccdc0234fabdd8d
Shimada Takayuki <taka@tsmd.jp>
2023-02-05 08:26:51 +0900

    scaffold guideline

---
+++ src/client/disclosure.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus";
+
+export class Disclosure extends Controller {
+	static targets = ["button", "content"];
+
+	toggle() {
+		this.expanded ? this.collapse() : this.expand();
+	}
+
+	expand() {
+		this.expanded = true;
+	}
+
+	collapse() {
+		this.expanded = false;
+	}
+
+	get expanded() {
+		return !this.contentTarget.hidden;
+	}
+
+	set expanded(expanded) {
+		this.buttonTarget.setAttribute("aria-expanded", String(expanded))
+		this.contentTarget.hidden = expanded ? false : "until-found";
+	}
+}
