ab01aba42ae43de2886885e23c00c3f028af50a5
Shimada Takayuki <taka@tsmd.jp>
2023-03-13 15:24:04 +0900

    prefix heading ids

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from "astro/config";
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+import addSlugToHeadingIds from "./src/plugins/add-slug-to-heading-ids.mjs";
 
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
@@ -8,5 +10,8 @@ export default defineConfig({
 	integrations: [mdx()],
 	experimental: {
 		assets: true,
+	},
+	markdown: {
+		rehypePlugins: [rehypeHeadingIds, addSlugToHeadingIds],
 	},
 });
---
+++ src/plugins/add-slug-to-heading-ids.mjs
@@ -0,0 +1,17 @@
+export default function () {
+	return function (tree, file) {
+		const { slug } = file.data.astro.frontmatter;
+		if (!slug) {
+			return;
+		}
+
+        walk(tree);
+
+		function walk(node) {
+			if (/^h[1-6]$/.test(node.tagName) && node.properties.id) {
+				node.properties.id = `${slug}-${node.properties.id}`;
+			}
+			node.children?.forEach(walk);
+		}
+	};
+}
